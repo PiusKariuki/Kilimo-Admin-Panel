f91efa80e3aa46b7ebb0d010c6095df6715d1a74
PiusKariuki <piuskariuki19@gmail.com>
2021-06-13 22:55:40 +0300

    secend commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+/*eslint-disable*/
+import React from "react";
+import PropTypes from "prop-types";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+// core components
+import styles from "assets/jss/material-dashboard-react/components/footerStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Footer(props) {
+  const classes = useStyles();
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <List className={classes.list}>
+
+          </List>
+        </div>
+      </div>
+    </footer>
+  );
+}
